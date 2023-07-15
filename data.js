d7df0a84c586e91260b58f9a7fe9410a67f8ff1c
lumislu <117718655+lumislu@users.noreply.github.com>
2023-07-15 15:40:36 +0800

    Update data.js

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const monsters = [
     englishName: "Boo",
     description:
       "她是一個可愛的兩歲小女孩，意外進入了怪獸世界。Boo的出現改變了Mike和Sulley的命運，並讓他們重新評估他們對怪獸和人類之間關係的看法。",
-    imgUrl: "./img/mons2.png",
+    imgUrl: "img/mons2.png",
   },
   {
     id: "7",
@@ -21,7 +21,7 @@ const monsters = [
     englishName: "Celia Mae",
     description:
       "Celia Mae在電影中展示了對Mike的支持和愛情，她經常鼓勵他追求夢想，並與他一起面對各種挑戰。",
-    imgUrl: "/img/mons7.webp",
+    imgUrl: "img/mons7.webp",
   },
   {
     id: "1",
@@ -29,7 +29,7 @@ const monsters = [
     englishName: "Mike Wazowski",
     description:
       "他真誠、幽默，與好友Sulley共同面對挑戰，彼此支持成長。Mike展現了勇氣、毅力和堅持夢想，同時傳遞友情和自我價值的重要訊息。他的獨特個性和風趣使他成為觀眾喜愛的角色。",
-    imgUrl: "/img/mons5.jpg",
+    imgUrl: "img/mons5.jpg",
   },
   {
     id: "4",
@@ -37,7 +37,7 @@ const monsters = [
     englishName: "Roz",
     description:
       "她是怪獸電力公司的秘書，以她冷酷的態度和嚴厲的聲音而聞名。Roz經常監視著Mike和Sulley，並對他們保持高度警惕。",
-    imgUrl: "/img/mons3.webp",
+    imgUrl: "img/mons3.webp",
   },
   {
     id: "5",
@@ -45,7 +45,7 @@ const monsters = [
     englishName: "Henry Waternoose",
     description:
       "沃特斯勒是一個堅定的領導者，相信怪獸世界的恐怖能源是不可或缺的。為了保護公司和怪獸世界免受能源危機的影響，他採取了一些極端的措施，包括與哈德斯合作進行不道德的實驗。",
-    imgUrl: "/img/mons4.webp",
+    imgUrl: "img/mons4.webp",
   },
   {
     id: "6",
@@ -53,15 +53,15 @@ const monsters = [
     englishName: "Randall Boggs",
     description:
       "像變色龍一樣可以根據環境改變顏色，達到隱身的效果，他是怪獸電力公司業績僅次於毛怪的驚叫專員，並因為長期屈居亞軍而對毛怪積怨已久。",
-    imgUrl: "/img/mons1.jpg",
+    imgUrl: "img/mons1.jpg",
   },
 ];
 
 const aboutImgs = [
-  { id: "1", imgUrl: "/img/about-1.jpg" },
-  { id: "2", imgUrl: "/img/about-2.jpg" },
-  { id: "3", imgUrl: "/img/about-3.jpg" },
-  { id: "4", imgUrl: "/img/about-4.jpg" },
-  { id: "5", imgUrl: "/img/about-5.jpg" },
-  { id: "6", imgUrl: "/img/about-6.jpg" },
+  { id: "1", imgUrl: "img/about-1.jpg" },
+  { id: "2", imgUrl: "img/about-2.jpg" },
+  { id: "3", imgUrl: "img/about-3.jpg" },
+  { id: "4", imgUrl: "img/about-4.jpg" },
+  { id: "5", imgUrl: "img/about-5.jpg" },
+  { id: "6", imgUrl: "img/about-6.jpg" },
 ];
